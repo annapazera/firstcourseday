cd50a2ff9630fb70200bf1988abc45d73817d232
Anna Pazera <anna.pazera@tlen.pl>
2016-07-25 12:58:55 +0200

    English all

--- src/it/morfoza/Main.java
+++ src/it/morfoza/Main.java
@@ -1,13 +1,16 @@
 package it.morfoza;
-
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args){
 
         int result= 2+2;
-        System.out.println("Hello Anna");
-        System.out.println("Have a nice course !!!!!!!!!!!");
-        System.out.println("Twój wynik to " + result );
+        System.out.println("Hello what's your name?");
+        String name;
+        Scanner input=new Scanner (System.in);
+        name=input.nextLine();
+        System.out.println("Your name is " + name + " !!!!!!!!!!!");
+        System.out.println("Your result is " + result );
         System.out.println("Goodbye");
 
 
