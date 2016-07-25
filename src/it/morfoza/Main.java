d0d7c4db0ee54f74e3d2d8b66fc2883f204f5075
Anna Pazera <anna.pazera@tlen.pl>
2016-07-25 11:46:25 +0200

    Change into English

--- src/it/morfoza/Main.java
+++ src/it/morfoza/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args){
         System.out.println("Hello Anna");
-        System.out.println("Miłej nauki Ci życzę !!!!!!!!!!!");
+        System.out.println("Have a nice course !!!!!!!!!!!");
 
         System.out.println("Goodbye");
         }
