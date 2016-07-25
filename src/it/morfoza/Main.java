b138db286c3dde1a9344dfa334dfe314dde8cd77
Anna Pazera <anna.pazera@tlen.pl>
2016-07-25 15:00:35 +0200

    dodawanie

--- src/it/morfoza/Main.java
+++ src/it/morfoza/Main.java
@@ -4,14 +4,24 @@ public class Main {
 
     public static void main(String[] args){
 
-        int result= 2+2;
+//        int result= 2+2;
         System.out.println("Hello what's your name?");
-        String name;
+
         Scanner input=new Scanner (System.in);
-        name=input.nextLine();
-        System.out.println("Your name is " + name + " !!!!!!!!!!!");
-        System.out.println(name + ", your result is " + result );
-        System.out.println("Goodbye");
+       String name=input.nextLine();
+
+        System.out.println("Podaj pierwszą liczbę");
+        int liczba1;
+        int liczba2;
+        liczba1=input.nextInt();
+        System.out.println("Podaj drugą liczbę");
+        liczba2=input.nextInt();
+        int wynik= liczba1+liczba2;
+
+
+        System.out.println(name +", suma Twoich liczb to " + wynik + " !!!!!!!!!!!");
+//        System.out.println(name + ", your result is " + result );
+//        System.out.println("Goodbye");
 
 
         }
