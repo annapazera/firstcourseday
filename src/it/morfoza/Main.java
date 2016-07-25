eecaa603b8eed3718a3785121aa63d60e9f2fd2c
Anna Pazera <anna.pazera@tlen.pl>
2016-07-25 11:56:38 +0200

    variable defined

--- src/it/morfoza/Main.java
+++ src/it/morfoza/Main.java
@@ -3,9 +3,13 @@ package it.morfoza;
 public class Main {
 
     public static void main(String[] args){
+
+        int result= 2+2;
         System.out.println("Hello Anna");
         System.out.println("Have a nice course !!!!!!!!!!!");
 
         System.out.println("Goodbye");
+
+
         }
 }
