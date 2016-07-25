76fcf64070a46218e8aff2b07c788ab37524d0f9
Anna Pazera <anna.pazera@tlen.pl>
2016-07-25 15:35:39 +0200

    switch case

--- src/it/morfoza/Main.java
+++ src/it/morfoza/Main.java
@@ -5,21 +5,51 @@ public class Main {
     public static void main(String[] args){
 
 //        int result= 2+2;
-        System.out.println("Hello what's your name?");
+        System.out.println("Hello, Jakie działanie chcesz wykonać?");
 
         Scanner input=new Scanner (System.in);
-       String name=input.nextLine();
+       String dzialanie=input.nextLine();
 
         System.out.println("Podaj pierwszą liczbę");
         int liczba1;
         int liczba2;
+
         liczba1=input.nextInt();
         System.out.println("Podaj drugą liczbę");
         liczba2=input.nextInt();
-        int wynik= liczba1+liczba2;
+
+        int wynik = liczba1 + liczba2;
+        int wynik1=liczba1-liczba2;
+        int wynik2=liczba1*liczba2;
+        int wynik3=liczba1/liczba2;
+
+        switch (dzialanie){
+            case "dodawanie":
+                System.out.println("Wynik dodawania  Twoich liczb to " + wynik + " !!!!!!!!!!!");
+                break;
+            case "odjmowanie":
+                System.out.println("Wynik odjmowania Twoich liczb to " + wynik1 + " !!!!!!!!!!!");
+                break;
+            case "mnożenie":
+                System.out.println("Wynik mnożenia Twoich liczb to " + wynik2 + " !!!!!!!!!!!");
+                break;
+            case "dzielenie":
+                System.out.println("Wynik dzielenia Twoich liczb to " + wynik3 + " !!!!!!!!!!!");
+                break;
+            default:
+
+                System.out.println("Nie mogę nic dla Ciebie zrobić  !!!!!!!!!!!");
 
 
-        System.out.println(name +", suma Twoich liczb to " + wynik + " !!!!!!!!!!!");
+//        if (dzialanie.equals("dodawanie")) {
+//
+//            System.out.println("Wynik dodawania  Twoich liczb to " + wynik + " !!!!!!!!!!!");
+//        }
+//        if (dzialanie.equals("odjmowanie")){
+//            int wynik2=liczba1-liczba2;
+//            System.out.println("Wynik odjmowania Twoich liczb to " + wynik1 + " !!!!!!!!!!!");
+        }
+
 //        System.out.println(name + ", your result is " + result );
 //        System.out.println("Goodbye");
 
