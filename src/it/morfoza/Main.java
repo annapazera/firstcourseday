cab2e9f221d8db9d7c953405769d37100e147ffe
Anna Pazera <anna.pazera@tlen.pl>
2016-07-25 11:45:30 +0200

    Goodbye

--- src/it/morfoza/Main.java
+++ src/it/morfoza/Main.java
@@ -5,5 +5,7 @@ public class Main {
     public static void main(String[] args){
         System.out.println("Hello Anna");
         System.out.println("Miłej nauki Ci życzę !!!!!!!!!!!");
+
+        System.out.println("Goodbye");
         }
 }
